7f5c291a1630d487cf2f270bb1d97bf324b6e71e
Tom C <tom.carlier@epitech.eu>
2024-05-03 13:25:50 +0200

    [refactor] -->  add the my_memset.c

--- include/my.h
+++ include/my.h
@@ -8,7 +8,18 @@
 #pragma once
     #define MY_H
     #include <unistd.h>
-    #include <stdlib.h>
-    #include <stdio.h>
+    #include "op.h"
+    #include <stdint.h>
     #include <stdbool.h>
     #include <stddef.h>
+
+typedef struct arena_s {
+    // process_t *champions;
+    uint8_t arena[MEM_SIZE];
+    uint8_t arena_copy[MEM_SIZE];
+    size_t cycles_to_die;
+    size_t nbr_live;
+    size_t total_cycles;
+} arena_t;
+
+void *my_memset(char *, char, size_t);
