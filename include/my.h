6dd1a732df60001c4a30d2117e4fee800443006d
Tom C <tom.carlier@epitech.eu>
2024-05-13 11:46:41 +0200

    [feat] -->  add the proccess_t structure in the header

--- include/my.h
+++ include/my.h
@@ -12,3 +12,19 @@
     #include <stdio.h>
     #include <stdbool.h>
     #include <stddef.h>
+    #include <stdint.h>
+    #include "op.h"
+
+typedef struct process_s {
+    int process_id;
+    char name[PROG_NAME_LENGTH + 1];
+    char comment[COMMENT_LENGTH+ 1];
+    uint8_t binary[MEM_SIZE + 1];
+    bool is_dead;
+    int cycles_to_wait;
+    size_t PC;
+    struct process_s *next;
+    bool carry;
+    uint8_t registers[REG_NUMBER][REG_SIZE];
+    size_t cycles_to_die;
+} process_t;
