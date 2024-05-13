98b255a02c7aa57a09b0f060a1770379fcad847f
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-13 15:57:25 +0200

    [feat] -->  added the function for update a cycle of the program

--- src/process/process_update.c
+++ src/process/process_update.c
@@ -7,3 +7,11 @@
 
 #include "my.h"
 
+int process_update(process_t *process)
+{
+    if (process->cycles_to_wait > 0)
+        process->cycles_to_wait -= 1;
+    if (process->is_dead == false)
+        process->cycles_to_die -= 1;
+    return 0;
+}
