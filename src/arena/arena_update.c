1a7c48a80022594d087ffc3de9112105d2070294
Tom C <tom.carlier@epitech.eu>
2024-05-14 16:43:31 +0200

    [feat] -->  add the arena_update()

--- src/arena/arena_update.c
+++ src/arena/arena_update.c
@@ -6,4 +6,20 @@
 */
 
 #include "my.h"
+#include "op.h"
 
+int arena_update(arena_t *arena)
+{
+    arena->total_cycles++;
+    if (arena->nbr_live >= NBR_LIVE) {
+        arena->cycles_to_die -= CYCLE_DELTA * (arena->nbr_live / NBR_LIVE);
+        arena->nbr_live = arena->nbr_live % NBR_LIVE;
+    }
+    for (process_t *tmp = arena->champions; tmp; tmp = tmp->next) {
+        process_update(tmp);
+        if (tmp->cycles_to_die == 0)
+            process_kill(tmp);
+    }
+    my_memcpy((void *)arena->arena, (void *)arena->arena_copy, MEM_SIZE);
+    return 0;
+}
