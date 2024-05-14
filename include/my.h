25c0e4360f1dbd63dc4353930c5e6701675072f4
Tom C <tom.carlier@epitech.eu>
2024-05-14 14:36:47 +0200

    [refactor] -->  add the process_change_register.c

--- include/my.h
+++ include/my.h
@@ -34,8 +34,7 @@ void process_destroy(process_t *);
 void process_kill(process_t *to_kill);
 int process_update(process_t *process);
 void process_move(process_t *process, int nbr_bytes);
-int process_change_register(process_t *process, size_t register_id,
-    size_t value);
+int process_change_register(process_t *process, size_t register_id, uint8_t *);
 size_t process_get_register(process_t *process, size_t register_id);
 void process_add(process_t *head, process_t *to_add);
 process_t *process_copy(process_t *head);
