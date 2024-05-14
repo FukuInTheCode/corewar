d223f853ab42653f3189738a5cfaf95c72bac04a
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 10:55:22 +0200

    [feat] -->  added a function for add a process to a linked list

--- src/process/process_add.c
+++ src/process/process_add.c
@@ -7,3 +7,16 @@
 
 #include "my.h"
 
+void process_add(process_t **head, process_t *to_add)
+{
+    process_t *current = *head;
+
+    if (*head == NULL)
+        *head = to_add;
+    else {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = to_add;
+    }
+    to_add->next = NULL;
+}
