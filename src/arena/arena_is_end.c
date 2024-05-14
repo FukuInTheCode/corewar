0380bb0a0f0ef2f61437003b0a08b5b8a2eab269
Tom C <tom.carlier@epitech.eu>
2024-05-14 16:33:24 +0200

    [feat] -->  add the arena_is_dead()

--- src/arena/arena_is_end.c
+++ src/arena/arena_is_end.c
@@ -7,3 +7,10 @@
 
 #include "my.h"
 
+bool arena_is_end(arena_t *arena)
+{
+    for (process_t *tmp = arena->champions; tmp; tmp = tmp->next)
+        if (!tmp->is_dead)
+            return false;
+    return true;
+}
