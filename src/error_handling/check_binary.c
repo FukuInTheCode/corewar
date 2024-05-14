5473fd884664cc92f5d1fcb6511aed8ba8b7e2d2
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-14 12:00:33 +0200

    [feat] -->  added the function check binary

---
+++ src/error_handling/check_binary.c
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2024
+** check_binary.c
+** File description:
+** check_binary.c
+*/
+
+#include "my.h"
+#include "op.h"
+
+static bool check_prog_size(header_t *data)
+{
+    if (data->prog_size < MEM_SIZE)
+        return true;
+    return false;
+}
+
+static bool check_comment(header_t *data)
+{
+    if (data->comment[COMMENT_LENGTH] == 0)
+        return true;
+    return false;
+}
+
+static bool check_prog_name(header_t *data)
+{
+    if (data->prog_name[PROG_NAME_LENGTH] == 0)
+        return true;
+    return false;
+}
+
+int check_binary(char *filename)
+{
+    FILE *file = fopen(filename, "rb");
+    header_t data;
+
+    if (file == NULL)
+        return 84;
+    if (fread(&data, sizeof(header_t), 1, file) != -1) {
+        fclose(file);
+        return 84;
+    }
+    if (!check_prog_name(&data) || !check_comment(&data) || !check_prog_size(&data)) {
+        fclose(file);
+        return 84;
+    }
+    fclose(file);
+    return 0;
+}
