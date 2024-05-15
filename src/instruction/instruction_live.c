23d5124a0d0e0dae81c9ec99cecddf495355de52
Tom C <tom.carlier@epitech.eu>
2024-05-15 23:21:33 +0200

    [feat] -->  add the live instruction fonction

---
+++ src/instruction/instruction_live.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** instruction_live.c
+** File description:
+** instruction_live.c
+*/
+
+#include "my.h"
+
+int instruction_live(process_t *process, arena_t *arena)
+{
+    uint16_t live_id = 0;
+
+    arena->nbr_live++;
+    live_id = (uint16_t)arena->arena[process->PC + 1];
+    for (process_t *tmp = arena->champions; tmp; tmp= tmp->next) {
+        if (tmp->process_id != live_id)
+            continue;
+        tmp->cycles_to_die = arena->cycles_to_die;
+    }
+    process->cycles_to_wait = 10;
+    return 0;
+}
