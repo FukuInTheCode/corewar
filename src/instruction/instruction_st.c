60719a91a04f929e7c598798e016dfb31d1fa711
Tom C <tom.carlier@epitech.eu>
2024-05-16 23:11:57 +0200

    [refactor] -->  refacto the st instruction

--- src/instruction/instruction_st.c
+++ src/instruction/instruction_st.c
@@ -8,34 +8,20 @@
 #include "my.h"
 #include "op.h"
 
-static int write_arena(process_t *process, arena_t *arena)
+void instruction_st(process_t *process, arena_t *arena)
 {
-    size_t first = process_get_arg_value(process, arena, 0);
-    size_t second = process_get_arg_value(process, arena, 1);
+    size_t arg1 = process_get_arg_type(process, arena, 0) & op_tab[1].type[0];
+    size_t arg2 = process_get_arg_type(process, arena, 1) & op_tab[1].type[1];
 
-    for (size_t i = 0; i < REG_SIZE; i++)
-        arena->arena_copy[(process->PC + second) % IDX_MOD + i] =
-            (first >> (2 * (REG_SIZE - i - 1))) & 0x03;
-    return 0;
-}
-
-int instruction_st(process_t *process, arena_t *arena)
-{
-    size_t first = process_get_arg_type(process, arena, 0);
-    size_t second = process_get_arg_type(process, arena, 1);
-
-    if (!(second == T_REG && first != 0 && first != T_IND)) {
-        process_move(process, 1);
-        return 0;
+    if (!arg1 || !arg2)
+        return process_move(process, 1);
+    arg1 = process_get_register(process,
+        process_get_arg_value(process, arena, 0));
+    if (arg2 == T_REG) {
+        arg2 = process_get_arg_value(process, arena, 1);
+        process_change_register(process, arg2, (uint8_t *)&arg1);
+        return;
     }
-    first = process_get_arg_value(process, arena, 0);
-    if (first == T_REG) {
-        process_change_register(process,
-        process_get_arg_value(process, arena, 1), (uint8_t *)&first);
-        process_move(process, 3);
-        return 0;
-    }
-    write_arena(process, arena);
-    process_move(process, 2 + process_get_arg_size(process, arena, 0));
-    return 0;
+    arg2 = process_get_arg_value(process, arena, 1);
+    arena_write(arena, process->PC + arg2, REG_SIZE, arg1);
 }
