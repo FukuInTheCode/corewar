9c732af131e659255e41c04972956904c7953dc1
Tom C <tom.carlier@epitech.eu>
2024-05-16 23:45:40 +0200

    [refactor] -->  change the return value of zjmp

--- src/instruction/instruction_zjmp.c
+++ src/instruction/instruction_zjmp.c
@@ -8,15 +8,12 @@
 #include "my.h"
 #include "op.h"
 
-int instruction_zjmp(process_t *process, arena_t *arena)
+void instruction_zjmp(process_t *process, arena_t *arena)
 {
     size_t to_go = 0;
 
-    if (!process->carry) {
+    if (!process->carry)
         process_move(process, 3);
-        return 0;
-    }
     to_go = arena_read(arena, process->PC + 1, IND_SIZE);
     process->PC = ((process->PC + to_go) % IDX_MOD) % MEM_SIZE;
-    return 0;
 }
