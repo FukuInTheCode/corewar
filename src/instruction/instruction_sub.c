a8a124ed6de8527e47252344f7269ed8fd238d57
Tom C <tom.carlier@epitech.eu>
2024-05-16 23:20:00 +0200

    [refactor] -->  refacto sub instruction

--- src/instruction/instruction_sub.c
+++ src/instruction/instruction_sub.c
@@ -8,24 +8,20 @@
 #include "my.h"
 #include "op.h"
 
-int instruction_sub(process_t *process, arena_t *arena)
-{
-    size_t first = process_get_arg_type(process, arena, 0);
-    size_t second = process_get_arg_type(process, arena, 1);
-    size_t third = process_get_arg_type(process, arena, 2);
-    size_t sum = 0;
 
-    if (first != T_REG || second != T_REG || third != T_REG) {
-        process_move(process, 1);
-        return 0;
-    }
-    first = process_get_arg_value(process, arena, 0);
-    second = process_get_arg_value(process, arena, 1);
-    third = process_get_arg_value(process, arena, 2);
-    sum = process_get_register(process, first) -
-        process_get_register(process, second);
-    process_change_register(process, third, (uint8_t *)&sum);
-    process_move(process, 4);
-    process->carry = !sum;
-    return 0;
+void instruction_sub(process_t *process, arena_t *arena)
+{
+    size_t arg1 = process_get_arg_type(process, arena, 0) & op_tab[4].type[0];
+    size_t arg2 = process_get_arg_type(process, arena, 1) & op_tab[4].type[1];
+    size_t arg3 = process_get_arg_type(process, arena, 2) & op_tab[4].type[2];
+
+    if (!arg1 || !arg2 || !arg3)
+        return process_move(process, 1);
+    arg1 = process_get_arg_value(process, arena, 0);
+    arg2 = process_get_arg_value(process, arena, 1);
+    arg3 = process_get_arg_value(process, arena, 2);
+    arg1 = process_get_register(process, arg1);
+    arg2 = process_get_register(process, arg2);
+    arg1 -= arg2;
+    process_change_register(process, arg3, (uint8_t *)&arg1);
 }
