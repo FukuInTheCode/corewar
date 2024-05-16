676c4a17ca353dd6a8e0e91fc5a1d2de13fe12f4
Tom C <tom.carlier@epitech.eu>
2024-05-16 11:29:52 +0200

    [refactor] -->  add the auto set of the waiting cycle of instructions

--- src/process/process_read_instruction.c
+++ src/process/process_read_instruction.c
@@ -6,12 +6,25 @@
 */
 
 #include "my.h"
+#include "op.h"
+
+static int set_wait(process_t *process, int code)
+{
+    for (size_t i = 0; op_tab[i].mnemonique; i++) {
+        if (op_tab[i].code != code)
+            continue;
+        process->cycles_to_wait = op_tab[i].nbr_cycles;
+        return 0;
+    }
+    return 0;
+}
 
 int process_read_instruction(process_t *process, arena_t *arena)
 {
     for (size_t i = 0; instructions[i].f; i++) {
         if (arena->arena[process->PC] == instructions[i].code) {
             instructions[i].f(process, arena);
+            set_wait(process, instructions[i].code);
             return 0;
         }
     }
