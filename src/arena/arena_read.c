e0afece37bb9f75809a643aa60edd43b4434ef0d
Tom C <tom.carlier@epitech.eu>
2024-05-16 22:43:58 +0200

    [fix] --> now the arena_read read from the base arean not the copy

--- src/arena/arena_read.c
+++ src/arena/arena_read.c
@@ -14,7 +14,7 @@ size_t arena_read(arena_t *arena, size_t pos, uint8_t n)
     size_t ret = 0;
 
     for (size_t i = 0; i < n; i++)
-        value[i] = (arena->arena_copy[(pos + i) % MEM_SIZE] >>
+        value[i] = (arena->arena[(pos + i) % MEM_SIZE] >>
             (2 * (n - i - 1))) & 0x03;
     my_memcpy((void *)&ret, (void *)value, n);
     return ret;
