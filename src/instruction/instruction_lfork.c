557ff0497837c6f255aa93c1b45f7065f4d1410c
Tom C <tom.carlier@epitech.eu>
2024-05-17 00:49:07 +0200

    [refactor] -->  add the lfork instruction

---
+++ src/instruction/instruction_lfork.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** instruction_lfork.c
+** File description:
+** instruction_lfork.c
+*/
+
+#include "my.h"
+
+void instruction_lfork(process_t *process, arena_t *arena)
+{
+    int64_t to_go = arena_read(arena, process->PC + 1, IND_SIZE);
+    process_t *forked = process_create();
+
+    if (!forked)
+        return;
+    process_add(&arena->champions, forked);
+    forked->cycles_to_wait = ops[14].nbr_cycles + 1;
+    process_move(forked, process->PC + to_go);
+    process_move(process, 3);
+}
