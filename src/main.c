b1dfad982b9fbdf7535ae9cf946fc3ec6b89b3fe
EmmaFrx <emma.farineaux@epitech.eu>
2024-05-17 17:19:41 +0200

    [refactor] -->  call the function for the "-h" with the value return

--- src/main.c
+++ src/main.c
@@ -9,5 +9,11 @@
 
 int main(int argc, char **argv)
 {
+    int return_value = display_help(argc, argv, "src/assets/help.txt");
+
+    if (return_value == 1)
+        return 0;
+    if (return_value == 84)
+        return 84;
     return handle_args(argc, argv);
 }
